58bbb344fba3d59391478727ae535456540b7abc
minhhoangzeno <minhhoangzeno@gmail.com>
2021-04-29 23:20:35 +0700

    demo

--- zoomx_front_end/src/page/HomePage.js
+++ zoomx_front_end/src/page/HomePage.js
@@ -7,7 +7,9 @@ export default function HomePage(){
         <div>
             <Header />
             Home Page
-            <Footer />
+            <div>
+                ALlo
+            </div>
         </div>
     )
 }
