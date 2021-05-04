e00984d0d295537eaeed2382456eee1fc5c10f5f
minhhoangzeno <minhhoangzeno@gmail.com>
2021-05-04 21:34:52 +0700

    create contact

---
+++ zoomx_front_end/src/component/contact/LeftContact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo1 from '../../image/home/country.png'
+export default function LeftContact(){
+    return(
+        <>
+            <div className="left__contact">
+                Left Contact
+                <img src={logo1} alt="" />
+            </div>
+        </>
+    )
+}
