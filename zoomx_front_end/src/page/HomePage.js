2ed759bb180f9659088c1a8250a1be5a62199d22
minhhoangzeno <minhhoangzeno@gmail.com>
2021-05-06 15:09:44 +0700

    update Hero Header + TimeLine

--- zoomx_front_end/src/page/HomePage.js
+++ zoomx_front_end/src/page/HomePage.js
@@ -4,6 +4,7 @@ import Header from '../component/share/Header';
 import MetaTags from 'react-meta-tags';
 import TimeLine from '../component/home/TimeLine';
 import '../style/home.scss'
+import HeroHeader from '../component/share/HeroHeader';
 export default function HomePage(){
     return(
         <div>
@@ -12,6 +13,7 @@ export default function HomePage(){
                 <meta name="description" content="Zoom X" />
             </MetaTags>
             <Header />
+            <HeroHeader />
             <div style={{width:'100%',height:500}}>
             Home Page
             </div>
