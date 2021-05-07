513cb5f762dbed34a9242cc7a841277200b9a03a
minhhoangzeno <minhhoangzeno@gmail.com>
2021-05-07 13:34:23 +0700

    update meta-tags heropage

--- zoomx_front_end/src/page/DetailRecruitmentPage.js
+++ zoomx_front_end/src/page/DetailRecruitmentPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Footer from '../component/share/Footer';
 import Header from '../component/share/Header';
+import HeroPage from '../component/share/HeroPage';
 export default function DetailRecruitmentPage(){
     return(
         <>
         <Header />
+        <HeroPage title="TUYỂN DỤNG" title_sub="TRANG CHỦ / TUYỂN DỤNG" />
+
             Chi tiết tuyển dụng
         <Footer />
         </>
--- zoomx_front_end/src/page/ZoomXPage.js
+++ zoomx_front_end/src/page/ZoomXPage.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { MetaTags } from 'react-meta-tags';
 import IntroduceComponent from '../component/introduce/IntroduceComponent';
 import PartnerComponent from '../component/introduce/PartnerComponent';
 import Footer from '../component/share/Footer';
 import Header from '../component/share/Header';
+import HeroPage from '../component/share/HeroPage';
 import '../style/introduce.scss'
 export default function ZoomXPage(){
     return(
         <>
+        <MetaTags>
+                <title>Giới thiệu</title>
+            </MetaTags>
         <Header />
+        <HeroPage title="VỀ ZOOMX HOTELS" title_sub="TRANG CHỦ / GIỚI THIỆU" />
         <IntroduceComponent />
         <PartnerComponent />
         <Footer />
