3f9f4c5af465c218761ba4049297aecf093a4bc1
haiducqn <haiduczxzx@gmail.com>
2021-05-08 02:01:12 +0700

    Update scss homePage

--- zoomx_front_end/src/component/home/News.js
+++ zoomx_front_end/src/component/home/News.js
@@ -1,9 +1,48 @@
 import React from "react";
+import view from "../../image/homePage/view.png";
+import view1 from "../../image/homePage/view1.png";
+import view2 from "../../image/homePage/view2.png";
 
 export default function News() {
   return (
     <>
-      <div>123123</div>
+      <div className="container-fluid">
+        <div className="news__item">
+          <p className="text__headline--news">WELCOME TO ZOOMX HOTELS</p>
+          <div className="empty__element--news"></div>
+          <p className="big__word-news">Tin tức</p>
+        </div>
+        <div className="row">
+          <div className="col-lg-4 news__block">
+            <img className="view__item" src={view} alt="#" />
+            <div className="news__content">
+              <p className="news__date"> Tháng 4 30, 2021</p>
+              <p className="view__item">
+                Kinh nghiệm du lịch Ninh Bình 4 ngày 3 đêm
+              </p>
+            </div>
+          </div>
+          <div className="col-lg-4 news__block">
+            <img className="view__item" src={view1} alt="#" />
+            <div className="news__content">
+              <p className="news__date"> Tháng 4 30, 2021</p>
+              <p className="view__item">
+                Kinh nghiệm du lịch Ninh Bình 4 ngày 3 đêm
+              </p>
+            </div>
+          </div>
+          <div className="col-lg-4 news__block">
+            <img className="view__item" src={view2} alt="#" />
+            <div className="news__content">
+              <p className="news__date"> Tháng 4 30, 2021</p>
+              <p className="view__item">
+                Kinh nghiệm du lịch Ninh Bình 4 ngày 3 đêm
+              </p>
+            </div>
+          </div>
+          <button className="btn__news">XEM THÊM</button>
+        </div>
+      </div>
     </>
   );
 }
