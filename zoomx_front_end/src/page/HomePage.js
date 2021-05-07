063ab88e88a36bf92cd571788b88816a9fcc9815
buihongthang <buihongthangneu@gmail.com>
2021-05-07 23:50:06 +0700

    push investment

--- zoomx_front_end/src/page/HomePage.js
+++ zoomx_front_end/src/page/HomePage.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import Footer from '../component/share/Footer';
-import Header from '../component/share/Header';
-import MetaTags from 'react-meta-tags';
-import TimeLine from '../component/home/TimeLine';
-import '../style/home.scss'
-import HeroHeader from '../component/share/HeroHeader';
-export default function HomePage(){
-    return(
-        <div>
-            <MetaTags>
-                <title>Trang chủ</title>
-                <meta name="description" content="Zoom X" />
-            </MetaTags>
-            <Header />
-            <HeroHeader />
-            <div style={{width:'100%',height:500}}>
-            Home Page
-            </div>
-            <TimeLine />
-            <Footer />
-        </div>
-    )
-}
+// import React from 'react';
+// import Footer from '../component/share/Footer';
+// import Header from '../component/share/Header';
+// import MetaTags from 'react-meta-tags';
+// import TimeLine from '../component/home/TimeLine';
+// import '../style/home.scss'
+// import HeroHeader from '../component/share/HeroHeader';
+// export default function HomePage(){
+//     return(
+//         <div>
+//             <MetaTags>
+//                 <title>Trang chủ</title>
+//                 <meta name="description" content="Zoom X" />
+//             </MetaTags>
+//             <Header />
+//             <HeroHeader />
+//             <div style={{width:'100%',height:500}}>
+//             Home Page
+//             </div>
+//             <TimeLine />
+//             <Footer />
+//         </div>
+//     )
+// }
--- zoomx_front_end/src/page/InvestmentPage.js
+++ zoomx_front_end/src/page/InvestmentPage.js
@@ -1,12 +1,80 @@
 import React from 'react';
 import Footer from '../component/share/Footer';
 import Header from '../component/share/Header';
-export default function InvestmentPage(){
-    return(
+import Img from '../image/investment/img.png';
+import '../style/investment.scss';
+
+export default function InvestmentPage() {
+    return (
         <>
-        <Header />
-            Lĩnh vực đầu tư
-        <Footer />
+            <Header />
+
+            <div className="investment-wrapper">
+                <div className="container-fluid">
+                    <div className="row investment__row--first">
+                        <div className="col-xl-7 col-lg-7 col-md-7 col-sm-7 col-12 col-img col-investment">
+                            <div className="img-wrapper">
+                                <img src={Img} alt="" />
+                            </div>
+                        </div>
+                        <div className="col-xl-5 col-lg-5 col-md-5 col-sm-5 col-12 col-content col-investment">
+                            <div className="content-wrapper">
+                                <h1>01/</h1>
+                                <h2>KHÁCH SẠN</h2>
+                                <p>Là một trong những thương hiệu dẫn đầu trong ngành khách sạn du lịch nghỉ dưỡng tại Việt Nam, Tập đoàn FLC sở hữu hệ…</p>
+                                <a className="read-more-btn">XEM THÊM</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row investment__row--second">
+                        <div className="col-xl-7 col-lg-7 col-md-7 col-sm-7 col-12 col-img col-investment">
+                            <div className="img-wrapper">
+                                <img src={Img} alt="" />
+                            </div>
+                        </div>
+                        <div className="col-xl-5 col-lg-5 col-md-5 col-sm-5 col-12 col-content col-investment">
+                            <div className="content-wrapper">
+                                <h1>02/</h1>
+                                <h2>KHÁCH SẠN</h2>
+                                <p>Là một trong những thương hiệu dẫn đầu trong ngành khách sạn du lịch nghỉ dưỡng tại Việt Nam, Tập đoàn FLC sở hữu hệ…</p>
+                                <a className="read-more-btn">XEM THÊM</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row investment__row--third">
+                        <div className="col-xl-7 col-lg-7 col-md-7 col-sm-7 col-12 col-img col-investment">
+                            <div className="img-wrapper">
+                                <img src={Img} alt="" />
+                            </div>
+                        </div>
+                        <div className="col-xl-5 col-lg-5 col-md-5 col-sm-5 col-12 col-content col-investment">
+                            <div className="content-wrapper">
+                                <h1>03/</h1>
+                                <h2>KHÁCH SẠN</h2>
+                                <p>Là một trong những thương hiệu dẫn đầu trong ngành khách sạn du lịch nghỉ dưỡng tại Việt Nam, Tập đoàn FLC sở hữu hệ…</p>
+                                <a className="read-more-btn">XEM THÊM</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row investment__row--fourth">
+                        <div className="col-xl-7 col-lg-7 col-md-7 col-sm-7 col-12 col-img col-investment">
+                            <div className="img-wrapper">
+                                <img src={Img} alt="" />
+                            </div>
+                        </div>
+                        <div className="col-xl-5 col-lg-5 col-md-5 col-sm-5 col-12 col-content col-investment">
+                            <div className="content-wrapper">
+                                <h1>04/</h1>
+                                <h2>KHÁCH SẠN</h2>
+                                <p>Là một trong những thương hiệu dẫn đầu trong ngành khách sạn du lịch nghỉ dưỡng tại Việt Nam, Tập đoàn FLC sở hữu hệ…</p>
+                                <a className="read-more-btn">XEM THÊM</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <Footer />
         </>
     )
 }
