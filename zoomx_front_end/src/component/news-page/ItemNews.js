c14c717f88b73a8396b77daaa384dac3caa5b49b
haiducqn <haiduczxzx@gmail.com>
2021-05-11 12:53:48 +0700

    fix responsive news-page

--- zoomx_front_end/src/component/news-page/ItemNews.js
+++ zoomx_front_end/src/component/news-page/ItemNews.js
@@ -6,7 +6,7 @@ export default function ItemNews() {
     <>
       <div className="container main__item__detail">
         <div className="row list__inner">
-          <div className="col-lg-6 col-md-12 wraper">
+          <div className="col-md-6 col-sm-12 wraper">
             <div className="block__detail">
               <img className="img__news" src={listItem} alt="#" />
               <div className="ingredient__item">
@@ -21,7 +21,7 @@ export default function ItemNews() {
               </div>
             </div>
           </div>
-          <div className="col-lg-6 col-md-12 wraper">
+          <div className="col-md-6 col-sm-12 wraper">
             <div className="block__detail">
               <img className="img__news" src={listItem} alt="#" />
               <div className="ingredient__item">
@@ -36,22 +36,7 @@ export default function ItemNews() {
               </div>
             </div>
           </div>
-          <div className="col-lg-6 col-md-12 col-sm-12 wraper">
-            <div className="block__detail">
-              <img className="img__news" src={listItem} alt="#" />
-              <div className="ingredient__item">
-                <div className="item__wrap">
-                  <p className="txt__small">TIPS & TRICK</p>
-                  <div className="empty__item"></div>
-                  <p className="txt__content">
-                    Kinh nghiệm du lịch Ninh Bình 4 ngày 3 đêm
-                  </p>
-                  <button className="btn__news">Xem thêm</button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-6 col-md-12 wraper">
+          <div className="col-md-6 col-sm-12 wraper">
             <div className="block__detail">
               <img className="img__news" src={listItem} alt="#" />
               <div className="ingredient__item">
