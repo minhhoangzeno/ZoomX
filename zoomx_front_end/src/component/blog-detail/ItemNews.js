6c5a225b3b8e675d52189bacaf807a76d96f11c9
buihongthang <buihongthangneu@gmail.com>
2021-05-13 04:21:53 +0700

    comiit

--- zoomx_front_end/src/component/blog-detail/ItemNews.js
+++ zoomx_front_end/src/component/blog-detail/ItemNews.js
@@ -4,9 +4,24 @@ import listItem from "../../image/newPage/listItem.png";
 export default function ItemNews() {
   return (
     <>
-      <div className="container-fluid main__item__detail">
+      <div className="container main__item__detail">
         <div className="row list__inner">
-          <div className="wrapper">
+          <div className="col-md-6 col-sm-12 wraper">
+            <div className="block__detail">
+              <img className="img__news" src={listItem} alt="#" />
+              <div className="ingredient__item">
+                <div className="item__wrap">
+                  <p className="txt__small">TIPS & TRICK</p>
+                  <div className="empty__item"></div>
+                  <p className="txt__content">
+                    Kinh nghiệm du lịch Ninh Bình 4 ngày 3 đêm
+                  </p>
+                  <button className="btn__news">Xem thêm</button>
+                </div>  
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6 col-sm-12 wraper">
             <div className="block__detail">
               <img className="img__news" src={listItem} alt="#" />
               <div className="ingredient__item">
@@ -21,22 +36,7 @@ export default function ItemNews() {
               </div>
             </div>
           </div>
-          <div className="wrapper">
-            <div className="block__detail">
-              <img className="img__news" src={listItem} alt="#" />
-              <div className="ingredient__item">
-                <div className="item__wrap">
-                  <p className="txt__small">TIPS & TRICK</p>
-                  <div className="empty__item"></div>
-                  <p className="txt__content">
-                    Kinh nghiệm du lịch Ninh Bình 4 ngày 3 đêm
-                  </p>
-                  <button className="btn__news">Xem thêm</button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="wrapper">
+          <div className="col-md-6 col-sm-12 wraper">
             <div className="block__detail">
               <img className="img__news" src={listItem} alt="#" />
               <div className="ingredient__item">
