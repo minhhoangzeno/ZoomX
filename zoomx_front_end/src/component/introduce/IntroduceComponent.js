9e492a0a4fc6bb61595ff53266c3199842aac10c
haiducqn <haiduczxzx@gmail.com>
2021-05-15 01:59:16 +0700

    fix all

--- zoomx_front_end/src/component/introduce/IntroduceComponent.js
+++ zoomx_front_end/src/component/introduce/IntroduceComponent.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import home from "../../image/home/home.png";
-import brackets from "../../image/zoomX/brackets.png";
+// import brackets from "../../image/zoomX/brackets.png";
 import hotel from "../../image/zoomX/hotel.png";
 import icon from "../../image/zoomX/icon.png";
 import icon1 from "../../image/zoomX/icon1.png";
@@ -12,7 +12,9 @@ export default function IntroduceComponent() {
     <>
       <div className="container-description">
         <div className="img-inner">
-          <img src={brackets} className="img-content" alt="#" />
+          {/* <img src={brackets} className="img-content" alt="#" />
+           */}
+          <div className="img-content">"</div>
         </div>
         <div className="detail-content">
           <p className="detail-content-block">
