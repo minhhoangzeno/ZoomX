545ceb8d2f9d72e689829d19864426f2e89d5e43
haiducqn <haiduczxzx@gmail.com>
2021-05-30 10:25:18 +0700

    commit fix

--- zoomx_front_end/src/lib/api/PartnerAPI.js
+++ zoomx_front_end/src/lib/api/PartnerAPI.js
@@ -1,28 +1,27 @@
-import { doGet, doPost, doDelete, doPut } from '../DataSource';
-import { useEffect, useState } from 'react';
-
+import { doGet, doPost, doDelete, doPut } from "../DataSource";
+import { useEffect, useState } from "react";
 
 export const usePartner = () => {
-    const [data, setData] = useState(null);
-    const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        getPartner()
-    }, []);
+  useEffect(() => {
+    getPartner();
+  }, []);
 
-    async function getPartner() {
-        const path = "/partner";
-        const headers = {
-            Accept: "*/*"
-        }
-        try {
-            var resp = await doGet(path, headers);
-            if (resp.status == 200) {
-                setData(resp.data)
-            }
-        } catch (e) {
-            setError(e)
-        }
+  async function getPartner() {
+    const path = "/partner";
+    const headers = {
+      Accept: "*/*",
+    };
+    try {
+      var resp = await doGet(path, headers);
+      if (resp.status == 200) {
+        setData(resp.data);
+      }
+    } catch (e) {
+      setError(e);
     }
-    return { data, error, loading: (data || error ? false : true) }
-}
+  }
+  return { data, error, loading: data || error ? false : true };
+};
