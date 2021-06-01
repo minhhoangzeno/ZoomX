740973893756b7ec24ecda7cd37eb48bb7525495
minhhoangzeno <minhhoangzeno@gmail.com>
2021-06-01 07:52:28 +0700

    commit

--- zoomx_front_end/src/page/BlogDetailPage.js
+++ zoomx_front_end/src/page/BlogDetailPage.js
@@ -5,12 +5,17 @@ import BlogDetail from '../component/blog-detail/BlogDetail';
 import '../style/blog-detail.scss';
 import Category from '../component/news-page/Category';
 import ItemNews from '../component/news-page/ItemNews';
+import HeroPage from '../component/share/HeroPage';
 
 
 export default function BlogDetailPage() {
     return (
         <>
             <Header />
+            <HeroPage
+                title="TIN TỨC - SỰ KIỆN"
+                title_sub="TRANG CHỦ / TIN TỨC - SỰ KIỆN"
+            />
             <div className="container-fluid blog-detail__page">
                 <div className="row blog-detail__row">
                     <div className="col-xl-8 col-6 col-12 main__news__page main-blog">
