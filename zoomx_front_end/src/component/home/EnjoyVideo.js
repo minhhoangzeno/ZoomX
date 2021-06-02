8f255186d2991828469006e68f3d25364a1619c5
haiducqn <haiduczxzx@gmail.com>
2021-06-03 00:41:29 +0700

    fix scss mobile

--- zoomx_front_end/src/component/home/EnjoyVideo.js
+++ zoomx_front_end/src/component/home/EnjoyVideo.js
@@ -1,20 +1,20 @@
 import React from "react";
 import hotel from "../../image/homePage/hotel.png";
 import play from "../../image/home/play.png";
-import videoMp4 from '../../image/video.mp4';
-import ReactPlayer from 'react-player';
+import videoMp4 from "../../image/video.mp4";
+import ReactPlayer from "react-player";
 import { ReactVideo } from "reactjs-media";
 export default function EnjoyVideo() {
   return (
     <>
       <div className="enjoy-video">
-            <ReactVideo
-                src={videoMp4}
-                poster={hotel}
-                primaryColor="red"
-                // other props
-            />
-        </div>
+        <ReactVideo
+          src={videoMp4}
+          poster={hotel}
+          primaryColor="red"
+          // other props
+        />
+      </div>
     </>
   );
 }
