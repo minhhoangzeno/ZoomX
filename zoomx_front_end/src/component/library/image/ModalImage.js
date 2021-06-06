7ab5422fc0a30e590de16c310ae2bc0c37ef8612
haiducqn <haiduczxzx@gmail.com>
2021-06-06 21:43:17 +0700

    !!!!!!!!!!!!!!

--- zoomx_front_end/src/component/library/image/ModalImage.js
+++ zoomx_front_end/src/component/library/image/ModalImage.js
@@ -1,28 +1,51 @@
-import React from 'react';
-import item from '../../../image/home/librarypage/itemOfList.jpg';
-import Modal from 'react-bootstrap/Modal'
+import React from "react";
+import Modal from "react-bootstrap/Modal";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import "swiper/swiper-bundle.css";
+import anh from "../../../image/modal/anh.jpg";
+import anh1 from "../../../image/modal/anh1.jpg";
+import anh2 from "../../../image/modal/anh2.jpg";
+import anh3 from "../../../image/modal/anh3.jpg";
+import anh4 from "../../../image/modal/anh4.jpg";
+import anh5 from "../../../image/modal/anh5.jpg";
 export default function ModalImage(props) {
-    return (
-        <>
-            <Modal
-                {...props}
-                size="lg"
-                aria-labelledby="contained-modal-title-vcenter"
-                centered
-            >
-                <div>
-                    <div onClick={props.onHide}
-                    style={{position:'fixed',top:'3%',right:'3%',color:'#FFF'}}
-                    >
-                        <svg style={{width:40,height:40}} viewBox="0 0 24 24">
-                            <path fill="currentColor" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
-                        </svg>
-                    </div>
-                    <div>
-                        <img src={item} alt="" />
-                    </div>
-                </div>
-            </Modal>
-        </>
-    )
+  return (
+    <>
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Swiper
+          tag="section"
+          wrapperTag="ul"
+          navigation
+          pagination
+          spaceBetween={0}
+          slidesPerView={1}
+        >
+          <SwiperSlide tag="li">
+            <img style={{ listStyle: "none" }} src={anh} alt="#" />
+          </SwiperSlide>
+          <SwiperSlide tag="li">
+            <img style={{ listStyle: "none" }} src={anh1} alt="#" />
+          </SwiperSlide>
+          <SwiperSlide tag="li">
+            <img style={{ listStyle: "none" }} src={anh2} alt="#" />
+          </SwiperSlide>
+          <SwiperSlide tag="li">
+            <img style={{ listStyle: "none" }} src={anh3} alt="#" />
+          </SwiperSlide>
+          <SwiperSlide tag="li">
+            <img style={{ listStyle: "none" }} src={anh4} alt="#" />
+          </SwiperSlide>
+          <SwiperSlide tag="li">
+            <img style={{ listStyle: "none" }} src={anh5} alt="#" />
+          </SwiperSlide>
+        </Swiper>
+      </Modal>
+    </>
+  );
 }
