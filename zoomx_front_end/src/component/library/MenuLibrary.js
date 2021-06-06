3c5082db2df3d2099f43d94404735725483257ad
haiducqn <haiduczxzx@gmail.com>
2021-06-06 17:07:36 +0700

    aaaaaaa

--- zoomx_front_end/src/component/library/MenuLibrary.js
+++ zoomx_front_end/src/component/library/MenuLibrary.js
@@ -1,38 +1,33 @@
-import React from 'react';
-import '../../style/library.scss'
-import iconNav from '../../image/home/librarypage/icon-menu.png'
+import React from "react";
+import "../../style/library.scss";
+import iconNav from "../../image/home/librarypage/icon-menu.png";
 
 export default function MenuLibrary() {
-    return (
-        <>
-            <div className="menu__wrapper">
-                <div className="container-fluid">
-                    <ul className="menu__library">
-                        <li>
-                            <img src={iconNav} alt="" />
-                            <a href="http://www.github.com">
-                                LOOK
-                            </a>
-                        </li>
-                        /
-                        <li>
-                            <img src={iconNav} alt="" />
-                            <a href="http://www.github.com">
-                                HÌNH ẢNH
-                                <div className="active-cpn"></div>
-                            </a>
-
-                        </li>
-                        /
-                        <li>
-                            <img src={iconNav} alt="" />
-                            <a href="http://www.github.com">
-                                VIDEO
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="menu__wrapper">
+        <div className="container-fluid">
+          <ul className="menu__library">
+            <li>
+              <img src={iconNav} alt="" />
+              <a href="http://www.github.com">LOOK</a>
+            </li>
+            /
+            <li>
+              <img src={iconNav} alt="" />
+              <a href="http://www.github.com">
+                HÌNH ẢNH
+                <div className="active-cpn"></div>
+              </a>
+            </li>
+            /
+            <li>
+              <img src={iconNav} alt="" />
+              <a href="http://www.github.com">VIDEO</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
