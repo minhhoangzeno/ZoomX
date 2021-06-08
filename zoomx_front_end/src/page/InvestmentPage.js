b4e8204060615bfef17ac9ed84f7ec8f5a933096
minhhoangzeno <minhhoangzeno@gmail.com>
2021-06-08 13:35:07 +0700

    commit

--- zoomx_front_end/src/page/InvestmentPage.js
+++ zoomx_front_end/src/page/InvestmentPage.js
@@ -3,20 +3,22 @@ import { MetaTags } from 'react-meta-tags';
 import Footer from '../component/share/Footer';
 import Header from '../component/share/Header';
 import HeroPage from '../component/share/HeroPage';
+import { useHeroInvestment } from '../lib/API';
+import '../style/investment.scss';
 import Investment from './../component/investment/Investment';
-import '../style/investment.scss'
+
 
 export default function InvestmentPage() {
-  
+    const { data } = useHeroInvestment()
     return (
         <>
             <MetaTags>
                 <title>Lĩnh vực đầu tư</title>
             </MetaTags>
             <Header />
-            <HeroPage title="LĨNH VỰC ĐẦU TƯ" title_sub="TRANG CHỦ / LĨNH VỰC ĐẦU TƯ" />
+            <HeroPage title={data?.[0]?.title} title_sub={data?.[0]?.label} imageBackground={data?.[0]?.imageCover?.url} />
 
-            <Investment  />
+            <Investment />
 
             <Footer />
         </>
