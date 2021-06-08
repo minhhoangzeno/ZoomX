36fbdc008cf9ed893f28f096fa9c63d7839b2577
buihongthang <buihongthangneu@gmail.com>
2021-06-09 01:28:38 +0700

    commit
    investment api

--- zoomx_front_end/src/component/investment/Investment.js
+++ zoomx_front_end/src/component/investment/Investment.js
@@ -1,68 +1,46 @@
 import React from 'react';
 import Img from '../../image/investment/img.png';
 import { useHistory } from 'react-router-dom';
-import InvestmentItem from './InvestmentItem'
+import { useEffect, useState } from 'react'
+import { doGet } from '../../lib/DataSource';
+import Item from './Item'
 import { useHeroInvestment } from '../../lib/API';
+
 export default function Investment() {
     let history = useHistory();
-    const { data } = useHeroInvestment()
-    console.log(data)
-    
+    const [data, setData] = useState(null); //tao 1 state data
+    const [error, setError] = useState(null); //tao 1 state error
+    const [loading, setLoading] = useState(false) //state loading
+    useEffect(() => {
+        getInvestment()
+    }, []); //vua vao page se chay luon
+
+    async function getInvestment() {
+        const path = "/investment";
+        const headers = {
+            Accept: "*/*"
+        }
+        try {
+            var resp = await doGet(path, headers);
+            if (resp.status === 200) {
+                setLoading(false)
+                setData(resp.data)
+            }
+        } catch (e) {
+            setError(e)
+            setLoading(true)
+        }
+    }
+
     return (
         <>
             <div className="investment-wrapper">
                 <div className="container-fluid">
-                    <InvestmentItem />
-                    {/* <div className="row investment__row--second">
-                        <div className="col-xl-7 col-lg-7 col-md-7 col-sm-7 col-12 col-img col-investment">
-                            <div className="img-wrapper">
-                                <div className="bg-inner"></div>
-                                <img src={Img} alt="" />
-                            </div>
-                        </div>
-                        <div className="col-xl-5 col-lg-5 col-md-5 col-sm-5 col-12 col-content col-investment">
-                            <div className="content-wrapper">
-                                <strong>02</strong><span>/</span>
-                                <h2>KHÁCH SẠN</h2>
-                                <p>Là một trong những thương hiệu dẫn đầu trong ngành khách sạn du lịch nghỉ dưỡng tại Việt Nam, Tập đoàn FLC sở hữu hệ…</p>
-                                <a href="http://www.github.com" className="read-more-btn">XEM THÊM</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row investment__row--third">
-                        <div className="col-xl-7 col-lg-7 col-md-7 col-sm-7 col-12 col-img col-investment">
-                            <div className="img-wrapper">
-                                <div className="bg-inner"></div>
-                                <img src={Img} alt="" />
-                            </div>
-                        </div>
-                        <div className="col-xl-5 col-lg-5 col-md-5 col-sm-5 col-12 col-content col-investment">
-                            <div className="content-wrapper">
-                                <strong>03</strong>
-                                <span>/</span>
-                                <h2>KHÁCH SẠN</h2>
-                                <p>Là một trong những thương hiệu dẫn đầu trong ngành khách sạn du lịch nghỉ dưỡng tại Việt Nam, Tập đoàn FLC sở hữu hệ…</p>
-                                <a href="http://www.github.com" className="read-more-btn">XEM THÊM</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row investment__row--fourth">
-                        <div className="col-xl-7 col-lg-7 col-md-7 col-sm-7 col-12 col-img col-investment">
-                            <div className="img-wrapper">
-                                <div className="bg-inner"></div>
-                                <img src={Img} alt="" />
-                            </div>
-                        </div>
-                        <div className="col-xl-5 col-lg-5 col-md-5 col-sm-5 col-12 col-content col-investment">
-                            <div className="content-wrapper">
-                                <strong>04</strong>
-                                <span>/</span>
-                                <h2>KHÁCH SẠN</h2>
-                                <p>Là một trong những thương hiệu dẫn đầu trong ngành khách sạn du lịch nghỉ dưỡng tại Việt Nam, Tập đoàn FLC sở hữu hệ…</p>
-                                <a href="http://www.github.com" className="read-more-btn">XEM THÊM</a>
-                            </div>
-                        </div>
-                    </div> */}
+                    {data?.map((item, index) => {
+                        return (
+                            <Item data={item} index={index + 1} />
+                        )
+                    })}
                 </div>
             </div>
         </>
---
+++ zoomx_front_end/src/component/investment/Item.js
@@ -0,0 +1,43 @@
+import React from 'react';
+export default function Item({ data, index }) {
+    return (
+        <>
+        
+            {index % 2 == 0 ? //kiểu lẻ và chẵn đảo nhau
+                <div className="row investment__row--first">
+                    <div className="col-xl-7 col-lg-7 col-md-7 col-sm-7 col-12 col-img col-investment">
+                        <div className="img-wrapper">
+                            <div className="bg-inner"></div>
+                            <img src={data.imageCover.url} alt="" />
+                        </div>
+                    </div>
+                    <div className="col-xl-5 col-lg-5 col-md-5 col-sm-5 col-12 col-content col-investment">
+                        <div className="content-wrapper">
+                            <strong>0{index}</strong><span>/</span>
+                            <h2>{data.investmentName}</h2>
+                            <p>{data.description}</p>
+                            <a href="http://www.github.com" className="read-more-btn" >XEM THÊM</a>
+                        </div>
+                    </div>
+                </div>
+                :
+                <div className="row investment__row--second">
+                    <div className="col-xl-7 col-lg-7 col-md-7 col-sm-7 col-12 col-img col-investment">
+                        <div className="img-wrapper">
+                            <div className="bg-inner"></div>
+                            <img src={data.imageCover.url} alt="" />
+                        </div>
+                    </div>
+                    <div className="col-xl-5 col-lg-5 col-md-5 col-sm-5 col-12 col-content col-investment">
+                        <div className="content-wrapper">
+                            <strong>0{index}</strong><span>/</span>
+                            <h2>{data.investmentName}</h2>
+                            <p>{data.description}</p>
+                            <a href="http://www.github.com" className="read-more-btn" >XEM THÊM</a>
+                        </div>
+                    </div>
+                </div>
+            }
+        </>
+    )
+}
