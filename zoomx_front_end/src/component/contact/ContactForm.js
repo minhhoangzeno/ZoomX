075183c9d160fad2669299dba5791ae3f0864c56
minhhoangzeno <minhhoangzeno@gmail.com>
2021-06-09 10:39:55 +0700

    commit contact

--- zoomx_front_end/src/component/contact/ContactForm.js
+++ zoomx_front_end/src/component/contact/ContactForm.js
@@ -5,7 +5,7 @@ import { useContact } from '../../lib/API';
 export default function ContactForm() {
   const [person, setPerson] = useState();
   const [content, setContent] = useState();
-  const {data} = useContact()
+  const { data } = useContact()
   const handlePerson = (e) => {
     const { name, value } = e.target;
     setPerson({
@@ -18,102 +18,103 @@ export default function ContactForm() {
     const headers = {
       "Content-Type": "multipart/form-data",
     };
-    const data = new FormData();
-    data.append("lastName", person?.lastName);
-    data.append("firstName", person?.firstName);
-    data.append("email", person?.email);
-    data.append("description", person?.description);
-    data.append("numberPhone", person?.numberPhone);
+    const formdata = new FormData();
+    formdata.append("lastName", person?.lastName);
+    formdata.append("firstName", person?.firstName);
+    formdata.append("email", person?.email);
+    formdata.append("description", person?.description);
+    formdata.append("numberPhone", person?.numberPhone);
     try {
-      let resp = await doPost(path, headers, data);
+      let resp = await doPost(path, headers, formdata);
       if (resp.status === 200) {
-        setContent("Ban da gui thanh cong!");
+        alert("Bạn đã gửi thông tin thành công!")
         setPerson({});
       }
     } catch (error) {
       console.log(error);
-      setContent("Vui long nhap lai");
+      alert("Thất bại! Vui lòng kiểm tra lại thông tin")
+
     }
   };
   return (
 
-    
-      <div className="contact__form">
-        <div className="contact__form__title">
-          <p>{data?.[0]?.title}</p>
-        </div>
-        <div className="contact__form__content">
-          <p>
+
+    <div className="contact__form">
+      <div className="contact__form__title">
+        <p>{data?.[0]?.title}</p>
+      </div>
+      <div className="contact__form__content">
+        <p>
           {data?.[0]?.content}
-          </p>
-        </div>
-        <div className="contact__form__main">
-          <form action="post">
-            <div className="form__main">
-              <div className="combo--item">
-                <div className="form--item">
-                  <p>Tên</p>
-                  <input
-                    type="text"
-                    name="lastName"
-                    onChange={handlePerson}
-                    value={person?.lastName}
-                  />
-                </div>
-                <div className="form--item">
-                  <p>Họ</p>
-                  <input
-                    type="text"
-                    name="firstName"
-                    onChange={handlePerson}
-                    value={person?.firstName}
-                  />
-                </div>
-              </div>
-
-              <div className="combo--item">
-                <div className="form--item">
-                  <p>Email</p>
-                  <input
-                    type="email"
-                    name="email"
-                    onChange={handlePerson}
-                    value={person?.email}
-                  />
-                </div>
-                <div className="form--item">
-                  <p>Số điện thoại</p>
-                  <input
-                    type="text"
-                    name="numberPhone"
-                    onChange={handlePerson}
-                    value={person?.numberPhone}
-                  />
-                </div>
-              </div>
-
-              <div className="form--mess">
-                <p>Tin nhắn</p>
-                <textarea
-                  name="description"
-                  id=""
-                  cols="30"
-                  rows="3"
-                  value={person?.description}
+        </p>
+      </div>
+      <div className="contact__form__main">
+        <form action="post">
+          <div className="form__main">
+            <div className="combo--item">
+              <div className="form--item">
+                <p>Tên</p>
+                <input
+                  type="text"
+                  name="lastName"
                   onChange={handlePerson}
-                ></textarea>
+                  value={person?.lastName}
+                />
+              </div>
+              <div className="form--item">
+                <p>Họ</p>
+                <input
+                  type="text"
+                  name="firstName"
+                  onChange={handlePerson}
+                  value={person?.firstName}
+                />
               </div>
             </div>
-          </form>
-        </div>
-        <div className="contact__form__button">
-          <button onClick={addPersonContact}>
-            <strong>GỬI</strong>
-          </button>
-          <p>{content}</p>
-        </div>
+
+            <div className="combo--item">
+              <div className="form--item">
+                <p>Email</p>
+                <input
+                  type="email"
+                  name="email"
+                  onChange={handlePerson}
+                  value={person?.email}
+                />
+              </div>
+              <div className="form--item">
+                <p>Số điện thoại</p>
+                <input
+                  type="text"
+                  name="numberPhone"
+                  onChange={handlePerson}
+                  value={person?.numberPhone}
+                />
+              </div>
+            </div>
+
+            <div className="form--mess">
+              <p>Tin nhắn</p>
+              <textarea
+                name="description"
+                id=""
+                cols="30"
+                rows="3"
+                value={person?.description}
+                onChange={handlePerson}
+              ></textarea>
+            </div>
+          </div>
+        </form>
       </div>
-    
+      <div className="contact__form__button">
+        <button onClick={addPersonContact}>
+          <strong>GỬI</strong>
+        </button>
+        <p>{content}</p>
+      </div>
+    </div>
+
   );
 }
 
