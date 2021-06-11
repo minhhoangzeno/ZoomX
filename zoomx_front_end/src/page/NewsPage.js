4c9f35064243aa148428e7fa672232d2282f9a95
minhhoangzeno <minhhoangzeno@gmail.com>
2021-06-11 08:27:30 +0700

    commit

--- zoomx_front_end/src/page/NewsPage.js
+++ zoomx_front_end/src/page/NewsPage.js
@@ -32,9 +32,9 @@ export default function NewsPage() {
   }, [])
   useEffect(() => {
     getSearch()
-  }, [activePage, categoryId])
+  }, [activePage, categoryId, isSearch])
   const getSearch = async () => {
-    if (!isSearch) {
+    if (isSearch) {
       setLoading(true);
       let path = `/blog-search?page=${activePage}&categoryId=${categoryId}&q=${textSearch}`;
       try {
@@ -65,7 +65,6 @@ export default function NewsPage() {
   }
   const handleSearch = () => {
     setIsSearch(true)
-    getSearch()
   }
   return (
     <>
@@ -78,7 +77,27 @@ export default function NewsPage() {
         title_sub="TRANG CHỦ / TIN TỨC - SỰ KIỆN"
       />
       <main className="main__container">
+        
         <div className="container-fluid main__home">
+        {isSearch && <>
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: 18 }}>
+            {data?.totalPage}  Kết quả hiện thị cho tìm kiếm &nbsp; <strong>{textSearch}</strong> &nbsp;
+                            <div onClick={() => setIsSearch(false)}>
+              <svg style={{ width: 35, height: 35, cursor: 'pointer' }} color="#65676b" viewBox="0 0 24 24"
+                onClick={async () => {
+                  await handleTextSearch("");
+                  setIsSearch(false);
+
+                }}
+              >
+                <path fill="currentColor" d="M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2C6.47,2 2,6.47 2,12C2,17.53 6.47,22 12,22C17.53,22 22,17.53 22,12C22,6.47 17.53,2 12,2M14.59,8L12,10.59L9.41,8L8,9.41L10.59,12L8,14.59L9.41,16L12,13.41L14.59,16L16,14.59L13.41,12L16,9.41L14.59,8Z" />
+              </svg>
+            </div>
+
+
+          </div>
+
+        </>}
           <div className="row item__home">
             <div className="col-xl-8 col-6 col-12 main__news__page" style={{ paddingLeft: 0 }}>
               <div className="container main__item__detail">
@@ -95,7 +114,9 @@ export default function NewsPage() {
               </div>
             </div>
             <div className="col-xl-4 main__news--cover">
-              <Category handleChangeCategory={handleChangeCategory} />
+              <Category handleChangeCategory={handleChangeCategory}
+                textSearch={textSearch} handleTextSearch={handleTextSearch} handleSearch={handleSearch}
+              />
             </div>
           </div>
         </div>
