09dd3b81cff6e184228e6e758b9a49bd7656a2f1
haiducqn <haiduczxzx@gmail.com>
2021-06-11 15:37:37 +0700

    aaaaaaaaaaaa

--- zoomx_front_end/src/component/home/News.js
+++ zoomx_front_end/src/component/home/News.js
@@ -1,6 +1,6 @@
-import React from "react";
 import Slider from "react-slick";
-import vn from "../../image/homePage/vn.png";
+import React from "react";
+import listItem from "../image/newPage/listItem.png";
 export default function News() {
   const settings = {
     // dots: true,
@@ -56,55 +56,63 @@ export default function News() {
   return (
     <div className="main__view--slider">
       <p className="text__headline--view">
-        {" "}
         WELCOME &nbsp; TO &nbsp; ZOOMX&nbsp; HOTELS
       </p>
       <div className="empty__element-view"></div>
       <p className="big__word-view">Tin tức</p>
       <Slider className="main__slider" {...settings}>
-        <div className="slider__item__cover">
-          <div className="item-block" style={{ backgroundImage: `url(${vn})` }}>
-            <div className="news-content">
-              <p className="item__content">Tháng 4 30, 2021</p>
-              <p className="item__content-cover">
-                Kinh nghiệm du lịch Ninh Bình 4 ngày 3 đêm
-              </p>
-              <div className="item__btn-main">XEM THÊM</div>
+        <div>
+          <div className="block__detail">
+            <img className="img__news" src={listItem} alt="#" />
+            <div className="ingredient__item">
+              <div className="item__wrap">
+                <p className="txt__small">TIPS & TRICK</p>
+                <div className="empty__item"></div>
+                <p className="txt__content">ád</p>
+                <button className="btn__news">XEM THÊM</button>
+              </div>
             </div>
           </div>
         </div>
 
-        <div className="slider__item__cover">
-          <div className="item-block" style={{ backgroundImage: `url(${vn})` }}>
-            <div className="news-content">
-              <p className="item__content">Tháng 4 30, 2021</p>
-              <p className="item__content-cover">
-                Kinh nghiệm du lịch Ninh Bình 4 ngày 3 đêm
-              </p>
-              <div className="item__btn-main">XEM THÊM</div>
+        <div>
+          <div className="block__detail">
+            <img className="img__news" src={listItem} alt="#" />
+            <div className="ingredient__item">
+              <div className="item__wrap">
+                <p className="txt__small">TIPS & TRICK</p>
+                <div className="empty__item"></div>
+                <p className="txt__content">ád</p>
+                <button className="btn__news">XEM THÊM</button>
+              </div>
             </div>
           </div>
         </div>
 
-        <div className="slider__item__cover">
-          <div className="item-block" style={{ backgroundImage: `url(${vn})` }}>
-            <div className="news-content">
-              <p className="item__content">Tháng 4 30, 2021</p>
-              <p className="item__content-cover">
-                Kinh nghiệm du lịch Ninh Bình 4 ngày 3 đêm
-              </p>
-              <div className="item__btn-main">XEM THÊM</div>
+        <div>
+          <div className="block__detail">
+            <img className="img__news" src={listItem} alt="#" />
+            <div className="ingredient__item">
+              <div className="item__wrap">
+                <p className="txt__small">TIPS & TRICK</p>
+                <div className="empty__item"></div>
+                <p className="txt__content">ád</p>
+                <button className="btn__news">XEM THÊM</button>
+              </div>
             </div>
           </div>
         </div>
-        <div className="slider__item__cover">
-          <div className="item-block" style={{ backgroundImage: `url(${vn})` }}>
-            <div className="news-content">
-              <p className="item__content">Tháng 4 30, 2021</p>
-              <p className="item__content-cover">
-                Kinh nghiệm du lịch Ninh Bình 4 ngày 3 đêm
-              </p>
-              <div className="item__btn-main">XEM THÊM</div>
+
+        <div>
+          <div className="block__detail">
+            <img className="img__news" src={listItem} alt="#" />
+            <div className="ingredient__item">
+              <div className="item__wrap">
+                <p className="txt__small">TIPS & TRICK</p>
+                <div className="empty__item"></div>
+                <p className="txt__content">ád</p>
+                <button className="btn__news">XEM THÊM</button>
+              </div>
             </div>
           </div>
         </div>
--- zoomx_front_end/src/component/news-page/ItemNews.js
+++ zoomx_front_end/src/component/news-page/ItemNews.js
@@ -5,25 +5,27 @@ export default function ItemNews({ data }) {
   let history = useHistory();
   return (
     <>
-      <div className="col-xl-6 col-lg-6 col-md-6 col-12 wrappers" onClick={() => history.push({
-        pathname: '/blog-detail',
-        state: data
-      })}>
+      <div
+        className="col-xl-6 col-lg-6 col-md-6 col-12 wrappers"
+        onClick={() =>
+          history.push({
+            pathname: "/blog-detail",
+            state: data,
+          })
+        }
+      >
         <div className="block__detail">
           <img className="img__news" src={data?.imageInfor?.url} alt="#" />
           <div className="ingredient__item">
             <div className="item__wrap">
               <p className="txt__small">TIPS & TRICK</p>
               <div className="empty__item"></div>
-              <p className="txt__content">
-                {data?.title}
-              </p>
+              <p className="txt__content">{data?.title}</p>
               <button className="btn__news">XEM THÊM</button>
             </div>
           </div>
         </div>
       </div>
-
     </>
   );
 }
